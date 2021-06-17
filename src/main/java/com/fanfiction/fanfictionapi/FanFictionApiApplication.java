35e4d61fb94b920c32c6cffae97f1fb887cf8e5b
Artem <hlopec1993@gmail.com>
2021-06-17 18:10:42 +0300

    Project initialization

---
+++ src/main/java/com/fanfiction/fanfictionapi/FanFictionApiApplication.java
@@ -0,0 +1,13 @@
+package com.fanfiction.fanfictionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanFictionApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FanFictionApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/fanfiction/fanfictionapi/FanFictionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fanfiction.fanfictionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FanFictionApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
