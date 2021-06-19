a167eb45eae72ff620bbcc00181837b75653f6e0
Artem <hlopec1993@gmail.com>
2021-06-19 13:33:48 +0300

    Added and configured logger
    Use log4j2

--- src/main/java/com/fanfiction/fanfictionapi/controller/FanficController.java
+++ src/main/java/com/fanfiction/fanfictionapi/controller/FanficController.java
@@ -1,6 +1,8 @@
 package com.fanfiction.fanfictionapi.controller;
 
 import com.fanfiction.fanfictionapi.Maps;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(Maps.FANFIC)
 public class FanficController {
-
+  
   @PostMapping
   public String test(){
     return "ok";
